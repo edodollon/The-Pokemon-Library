fe3cf2fbb3464ddbed3d05254514b1a5a936afc7
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-12-02 16:38:23 -0800

    menu overlay styling/responsive breakpoints

--- components/Menu.js
+++ components/Menu.js
@@ -18,7 +18,7 @@ function Menu({ openMenu }) {
                 <span>01</span> Sets
               </li>
             </Link>
-            <Link href="/">
+            <Link href="/Search">
               <li>
                 <span>02</span> Search
               </li>
@@ -30,6 +30,14 @@ function Menu({ openMenu }) {
             </Link>
           </ul>
         </nav>
+      </div>
+
+      <div className={classes["secondary-nav--container"]}>
+        <p className={classes["blurb"]}>
+          This space is currently being built. For now if you would like to work
+          together or for any inquiries about the project connect with me below!
+        </p>
+        <p className={classes["contact--button"]}>Contact</p>
       </div>
     </div>
   );
